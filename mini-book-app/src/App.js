388457e146324adee4742fa266519c2f37caad50
Erick Rodriguez <erickrod54@gmail.com>
2021-12-12 00:58:39 -0500

    this App js file is the second props version of mini-book-app

--- mini-book-app/src/App.js
+++ mini-book-app/src/App.js
@@ -1,19 +1,40 @@
 import './App.css';
 
-/**This App js file is the props version of the minibook app, and 
+/**This App js file is the second props version of the minibook app, and 
  * will allow to use the same component Book with different book data
  */
 
-const title = 'The Mastery of Life: A Toltec Guide to Personal Freedom'
-const author = 'don Miguel Ruiz Jr'
-const img = 'https://m.media-amazon.com/images/I/51-Jv++qMdL._SY300_.jpg '
+/**here i will create two different books 'firstBook' and 'secondBook' */
+const firstBook = {
+  img : 'https://m.media-amazon.com/images/I/51-Jv++qMdL._SY300_.jpg ',
+  title: 'The Mastery of Life: A Toltec Guide to Personal Freedom',
+  author: 'don Miguel Ruiz Jr'
+}
+
+const secondBook = {
+  img : 'https://images-na.ssl-images-amazon.com/images/I/51NsRhI4llL._SX348_BO1,204,203,200_.jpg ',
+  title: 'The Four Agreements Companion Book: Using the Four Agreements to Master the Dream of Your Life (Toltec Wisdom)',
+  author: ' don Miguel Ruiz (Author), Janet Mills'
+}
 
 function App() {
   return (
     <section className='booklist'>
       {/**this attributes in the Book component represent props*/}
-      <Book job='developer'/>
-      <Book title='random title' number='22'/>
+
+      {/**Using the generic Book component i render
+       * using 'img','title','author' props for each 
+       * different book
+       */}
+      <Book 
+          img={firstBook.img}
+          title={firstBook.title}
+          author={firstBook.author}
+          />
+      <Book img={secondBook.img}
+            title={secondBook.title}
+            author={secondBook.author}
+            />
     </section>
   );
 }
@@ -26,17 +47,12 @@ const Book = (props) => {
   console.log(props)
   return (
     <article className='book'>
-    {/**Here i made jsx more dynamic adding variables that i can change
-     * or use methods on them like the case of toUpperCase
-     */}
+    {/**Here i access the props using 'props.img', 'props.title', 'props.author' */}
     <img 
-        src={img}
+        src={props.img}
         alt="" />
-    <h1>{title}</h1>
-    <h4>{author}</h4>
-    <p>{props.job}</p>
-    <p>{props.title}</p>
-    <p>{props.number}</p>
+    <h1>{props.title}</h1>
+    <h4>{props.author}</h4>
   </article>
   );
 }
