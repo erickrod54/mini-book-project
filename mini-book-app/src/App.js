0b7af2910c69297ba25712efeaf1932b4faf18b1
Erick Rodriguez <erickrod54@gmail.com>
2021-12-11 20:24:53 -0500

    this is the first props version of mini-book-app,in this version is well explained how props are being accesed

--- mini-book-app/src/App.js
+++ mini-book-app/src/App.js
@@ -1,33 +1,42 @@
 import './App.css';
 
+/**This App js file is the props version of the minibook app, and 
+ * will allow to use the same component Book with different book data
+ */
+
+const title = 'The Mastery of Life: A Toltec Guide to Personal Freedom'
+const author = 'don Miguel Ruiz Jr'
+const img = 'https://m.media-amazon.com/images/I/51-Jv++qMdL._SY300_.jpg '
+
 function App() {
   return (
-    <section>
-      {/**here we render the component
-       * that has inside nested components
-       * and will act as a booklist
-       */}
-      <Book />
-
+    <section className='booklist'>
+      {/**this attributes in the Book component represent props*/}
+      <Book job='developer'/>
+      <Book title='random title' number='22'/>
     </section>
   );
 }
 
-const Book = () => {
-  /**here i create variables to facilate quickly changes in my code */
-  const title = 'The Mastery of Life: A Toltec Guide to Personal Freedom'
-  const author = 'don Miguel Ruiz Jr'
+/**the arguments here for the book component are the props from above
+ * and this way they're being passed to be accesed and render directly in
+ * the respective component making our component unique
+ */
+const Book = (props) => {
+  console.log(props)
   return (
     <article className='book'>
     {/**Here i made jsx more dynamic adding variables that i can change
      * or use methods on them like the case of toUpperCase
      */}
     <img 
-        src="https://m.media-amazon.com/images/I/51-Jv++qMdL._SY300_.jpg" 
+        src={img}
         alt="" />
     <h1>{title}</h1>
-    <h4>{author.toUpperCase()}</h4>
-    <p>{6 + 6}</p>
+    <h4>{author}</h4>
+    <p>{props.job}</p>
+    <p>{props.title}</p>
+    <p>{props.number}</p>
   </article>
   );
 }
