9161dc2761cf1045b59be714e355cad8be06526c
Erick Rodriguez <erickrod54@gmail.com>
2021-12-13 19:55:02 -0500

    fourth props version of App js file, here are lines related to children props, this version shows how to access them

--- mini-book-app/src/App.js
+++ mini-book-app/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
 
-/**This App js file is the third props version of the minibook app, and 
- * will cover lines related with how i destructured props
+/**This App js file is the fourth props version of the minibook app, and 
+ * will cover lines related with children props, this first version of children
+ * props i can access them but i can't whatch them in the JavaConsole, to do 
+ * so i have to access them destructuring the props as a const, i'll do it
+ * in fifth App js props version
  */
 
 /**here i will create two different books 'firstBook' and 'secondBook' */
@@ -31,11 +34,28 @@ function App() {
           img={firstBook.img}
           title={firstBook.title}
           author={firstBook.author}
-          />
+          >
+      {/**here between an opening and closing tag of Book component i place
+       * a 'firstBook' children notice that children *can be buttons, forms, or whole 
+       * apps*
+       */}
+       <p>-firstBook children- Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, 
+         sed do eiusmod tempor incididunt ut labore et dolore 
+         magna aliqua.</p>
+      </Book>
       <Book img={secondBook.img}
             title={secondBook.title}
             author={secondBook.author}
-            />
+            >
+      {/**here between an opening and closing tag of Book component i place
+       * a secondBook children
+       */}
+       <p>-secondBook children- Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, 
+         sed do eiusmod tempor incididunt ut labore et dolore 
+         magna aliqua.</p>
+        </Book>
     </section>
   );
 }
@@ -46,20 +66,23 @@ function App() {
  */
 
 
-/**this is the second way to destructure props, directly passing props
- * throught the component*/
-const Book = ({img, title, author }) => {
+/**children are on props object so i access them as a props */
+const Book = ({img, title, author, children }) => {
   /**this is the first way in how i can destructure props */
   //const {img, title, author } = props
   console.log({img, title, author })
   return (
     <article className='book'>
     {/**Here i access the props using 'props.img', 'props.title', 'props.author' */}
-    <img 
+    <img style={{width: 214, height: 300}}
         src={img}
         alt="" />
     <h1>{title}</h1>
     <h4>{author}</h4>
+    {/**i can place children wherever i need it to render, for example here
+     * think about it as a description
+     */}
+     {children}
   </article>
   );
 }
