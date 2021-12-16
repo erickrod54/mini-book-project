d7c4e862ef11a3816b8571595b0072bcf9d28c57
Erick Rodriguez <erickrod54@gmail.com>
2021-12-15 21:28:23 -0500

    version 2 (two) of refactor data, this add an id to each book inside the books JSON object, use a key prop in Book component, and spread operator for book props

--- mini-book-app/src/App.js
+++ mini-book-app/src/App.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import './App.css';
 
-/**This App js version 1 (one) will focus in refactor the data, and how i map over 
+/**This App js version 2 (two) will focus in refactor the data, and how i map over 
  * an JSON object for hte case of the books object */
 
-/**here i will create an array of books, with two books, and i can easily keep
- * adding more books, this version 1 (one) of refactor data, has a warning
+/** this version fix a warning 
+ *  
+ *  'index.js:1 Warning: Each child in a list should have a unique "key" prop.' 
  * 
- *  'index.js:1 Warning: Each child in a list should have a unique "key" prop.'
- * 
- * this will be fixed in the version 2 (two) of refactor the data
+ * we have to add an unique 'id' to each 'book' in my 'books' JSON object 
  */
+
 const books = [
   {
+    id:1,
     img : 'https://m.media-amazon.com/images/I/51-Jv++qMdL._SY300_.jpg ',
     title: 'The Mastery of Life: A Toltec Guide to Personal Freedom',
     author: 'don Miguel Ruiz Jr'
   },
   {
+    id:2,
     img : 'https://images-na.ssl-images-amazon.com/images/I/51NsRhI4llL._SX348_BO1,204,203,200_.jpg ',
     title: 'The Four Agreements Companion Book: Using the Four Agreements to Master the Dream of Your Life (Toltec Wisdom)',
     author: ' don Miguel Ruiz (Author), Janet Mills'
   },
   {
+    id:3,
     img : 'https://m.media-amazon.com/images/I/41lAPQqRZOL.jpg',
     title: 'Eros',
     author: ' Barbara Emrys (Author), Miguel Ruiz (Author)'
@@ -37,8 +40,11 @@ function App() {
     <section className='bookList'>{books.map((book) => {
       return (
         /** this way i send the 'books' data as a props and a get them in the
-         * 'Book' component */
-        <Book book={book}></Book>
+         * 'Book' component, in this new approach i'll do it with spread operator*/
+
+        /**as part of the book i have to set the 'key' prop that will be
+         * 'book.id' */
+        <Book key={book.id}{...book}></Book>
       );
     })}
     </section>
@@ -46,10 +52,12 @@ function App() {
 }
 
 const Book = (props) => {
-  /**i set the props with the prop that i'm looking for this case 'book' and 
-   * will be set as 'props.book'
+  /**i set the props with the prop that i'm looking for this case 'book' in this
+   * approach as i did with spread operator '{...book}', is already passing 
+   * everything that a book has 'img', 'title', 'author' and if i add will pass 
+   * additional props  
    */
-  const {img, title, author } = props.book;
+  const {img, title, author } = props;
   console.log(props);
   return (
     <article className='book'>
