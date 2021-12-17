d868d9e7face585a8e9ff49a22aa4c1a83b6bb30
Erick Rodriguez <erickrod54@gmail.com>
2021-12-16 19:14:02 -0500

    this App js file version cover topics related with 'React Basic Events', called also 'synthetic events'

--- mini-book-app/src/App.js
+++ mini-book-app/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 
-/**This App js version 2 (two) will focus in refactor the data, and how i map over 
- * an JSON object for hte case of the books object */
-
-/** this version fix a warning 
- *  
- *  'index.js:1 Warning: Each child in a list should have a unique "key" prop.' 
+/**This App js 'React Basic Events', events in react are called 'synthetic events' 
+ * that's because mimic html as JSX, and this version will focus in how can i handle 
+ * events with react, i will work with two events
  * 
- * we have to add an unique 'id' to each 'book' in my 'books' JSON object 
+ *    onMouseOver()
+ *    onClick()
+ * 
+ * reference https://reactjs.org/docs/events.html
  */
 
 const books = [
@@ -55,19 +55,61 @@ const Book = (props) => {
   /**i set the props with the prop that i'm looking for this case 'book' in this
    * approach as i did with spread operator '{...book}', is already passing 
    * everything that a book has 'img', 'title', 'author' and if i add will pass 
-   * additional props  
+   * additional props, if i want selectively pass props i can pass direct to 
+   * 'Book' component like this
+   * 
+   *     {img, title, author }  
    */
+
   const {img, title, author } = props;
   console.log(props);
+
+  /**More Complex Example is a handler that i can use to pass whatever i need
+   * from the 'books' JSON object
+   */
+   const complexExample = (author) => {
+        console.log(author)
+   }
+
+  /**here i make the handler */
+  const clickHandler = (e) => {
+    /**this 'e' is a prop and when we apply 'e.target' refers the jsx element
+     * in this case that contain the event, but i can use it to refer to any
+     * element that i want to 'target'
+    */
+    console.log(e);
+    console.log(e.target)
+    alert('hello world')
+  }
   return (
-    <article className='book'>
+
+    /**this onMouseOver event every time that the mouse is over something trigger
+     * the event and count the over, for this case print in javaConsole the title
+     */
+    <article className='book' onMouseOver={() => {
+      console.log(title)
+    }}>
     {/**Here i access the props using 'props.img', 'props.title', 'props.author' */}
-    <img style={{width: 214, height: 300}}
+
+    {/**this syntethic onClick event print on javaConsole the book title just
+     * by doing click, this way is called 'inline event' */}
+    <img onClick={() => console.log(title)} style={{width: 214, height: 300}}
         src={img}
         alt="" />
     <h1>{title}</h1>
     <h4>{author}</h4>
 
+    {/** i can apply an event to any jsx element, this case i'll work with buttons*/}
+    {/**i need attribute, and make an eventHandler, this is called 
+     * 'Event as a reference' */}
+    <button 
+           type='button' 
+           onClick={clickHandler}
+           > Event as a reference </button>
+    <button 
+           type='button' 
+           onClick={() => complexExample(author)}
+           > More Complex Example </button>
   </article>
   );
 }
